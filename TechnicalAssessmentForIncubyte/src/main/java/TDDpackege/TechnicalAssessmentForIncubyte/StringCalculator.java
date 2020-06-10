8fade5248a2946a39c3e9734da807933e8671049
rakes <rakes@LAPTOP-2HOTL6JN>
2020-06-10 11:33:03 +0530

    add implementation code to handle two or more element

--- TechnicalAssessmentForIncubyte/src/main/java/TDDpackege/TechnicalAssessmentForIncubyte/StringCalculator.java
+++ TechnicalAssessmentForIncubyte/src/main/java/TDDpackege/TechnicalAssessmentForIncubyte/StringCalculator.java
@@ -9,6 +9,18 @@ public class StringCalculator {
 		else if(numbers.length() == 1)
 		{
 			return Integer.parseInt(numbers);
+		}else if(numbers.length() >= 2 )
+		{
+			int sum = 0; 
+			for(int i = 0; i < numbers.length(); i++) 
+			{ 
+				char ch = numbers.charAt(i); 
+				if (Character.isDigit(ch)) 
+					sum += Integer.parseInt(String.valueOf(ch)); 
+
+			} 
+		
+			 return sum ; 
 		}
 		return -1;
 		
