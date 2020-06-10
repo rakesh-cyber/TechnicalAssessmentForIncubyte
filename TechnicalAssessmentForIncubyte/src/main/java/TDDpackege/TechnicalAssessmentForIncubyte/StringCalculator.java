f3250deaa5780fb7db872f3d3b43e2c18330af2d
rakes <rakes@LAPTOP-2HOTL6JN>
2020-06-10 11:27:38 +0530

    add implementation code for handle test method to only one element

--- TechnicalAssessmentForIncubyte/src/main/java/TDDpackege/TechnicalAssessmentForIncubyte/StringCalculator.java
+++ TechnicalAssessmentForIncubyte/src/main/java/TDDpackege/TechnicalAssessmentForIncubyte/StringCalculator.java
@@ -6,6 +6,10 @@ public class StringCalculator {
 	{
 		if(numbers.length() == 0 )
 			return 0;
+		else if(numbers.length() == 1)
+		{
+			return Integer.parseInt(numbers);
+		}
 		return -1;
 		
 	}
